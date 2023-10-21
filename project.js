e40875a4826743c964e762d031dab0d958eeded5
mrphillips1 <90735396+mrphillips1@users.noreply.github.com>
2023-10-21 09:43:15 -0400

    Update project.js

--- project.js
+++ project.js
@@ -1,12 +1,47 @@
 
+const salesData = [
+  { name: "East (Matt)", sales: [45.00, 50.00, 47.00] },
+  { name: "West (Mike)", sales: [95.00, 73.00, 40.00] },
+  { name: "Westshore (Mark)", sales: [10.00, 32.00, 24.00] },
+  { name: "Metro (Milt)", sales: [86.00, 74.00, 80.00] },
+];
+
 function salesByPerson() {
-   alert("Display data in this alert box.");
+  let result = "";
+  for (const entry of salesData) {
+    const totalSales = entry.sales.reduce((acc, monthSale) => acc + monthSale, 0);
+    result += `${entry.name}: $${totalSales.toFixed(2)}\n`;
+  }
+  alert("Total Sales by Person/Campus (Q1):\n" + result);
 }
 
 function bestAndWorst() {
-   alert("Display data in this alert box.");
+  let bestPerson = "";
+  let worstPerson = "";
+  let bestSales = -1;
+  let worstSales = Number.MAX_VALUE;
+
+  for (const entry of salesData) {
+    const totalSales = entry.sales.reduce((acc, monthSale) => acc + monthSale, 0);
+    if (totalSales > bestSales) {
+      bestSales = totalSales;
+      bestPerson = entry.name;
+    }
+    if (totalSales < worstSales) {
+      worstSales = totalSales;
+      worstPerson = entry.name;
+    }
+  }
+
+  alert(`Best Salesperson: ${bestPerson} ($${bestSales.toFixed(2})\nWorst Salesperson: ${worstPerson} ($${worstSales.toFixed(2})`);
 }
 
 function commissionsByMonth() {
-   alert("Display data in this alert box.");
+  let result = "";
+  for (const entry of salesData) {
+    const totalSales = entry.sales.reduce((acc, monthSale) => acc + monthSale, 0);
+    const totalCommissions = (totalSales * 0.13).toFixed(2);
+    result += `${entry.name}: $${totalCommissions}\n`;
+  }
+  alert("Total Commissions by Person/Campus (Q1):\n" + result);
 }
